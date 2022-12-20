4b266f79a783d368064565f8f86701631a5e43dd
kodehodekim <kim.kodehode@gmail.com>
2022-12-20 10:34:13 +0100

    Modified code

--- Modul 3 JS/UnitConverter/index.js
+++ Modul 3 JS/UnitConverter/index.js
@@ -1,44 +1,53 @@
 // prettier-ignore
 
+// Grabbing ID's from HTML
 const inputField = document.getElementById("input");
 const convertButton = document.getElementById("convertButton");
-const lengthOutput = document.getElementById("length");
-const massOutput = document.getElementById("mass");
-const volumeOutput = document.getElementById("volume");
+// Length - Meter / Feet
+const metertoFeetEl = document.getElementById("meterToFeetElement");
+const feetToMeterEl = document.getElementById("feetToMeterElement");
+// Volume - Liters / Gallons
+const literToGallonEl = document.getElementById("litersToGallonsElement");
+const gallonsToLitresEl = document.getElementById("gallonsToLitresElement");
+// Mass - Kilo / Pound
+const kiloToPoundEl = document.getElementById("kiloToPoundElement");
+const poundToKiloEl = document.getElementById("poundsToKiloElement");
 
+// Calculation values
 const meterToFeet = 3.281;
 const literToGallon = 0.264;
 const kgToPound = 2.204;
 
+// Eventlistener to run function when enter is pressed.
 inputField.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
-    event.preventDefault();
     document.getElementById("convertButton").click();
   }
 });
-
+// EventListener for click on button and change the HTML.
 convertButton.addEventListener("click", function () {
-  let baseValue = inputField.value;
+  const baseValue = inputField.value;
   // Meter to feet and reversed.
-  lengthOutput.innerHTML = `<h3 id="lengthEl">Length (Meter/Feet)</h3>
-        <h4><span id="meterToFeet">${baseValue} meters = ${
+  metertoFeetEl.textContent = `${baseValue} Meters = ${
     baseValue * meterToFeet
-  } feet</span> | <span id="feetToMeter"> ${baseValue} feet = ${
+  } Feet.`;
+  feetToMeterEl.textContent = `${baseValue} Feet = ${
     baseValue / meterToFeet
-  } meters</span>`;
+  } Meters.`;
+
   // Liters to Gallons and reversed.
-  volumeOutput.innerHTML = `<h3 id="volumeEl">Volume (Liters/Gallons)</h3>
-  <h4><span id="litersToGallons">${baseValue} liters = ${
+  literToGallonEl.textContent = `${baseValue} Liters = ${
     baseValue * literToGallon
-  } 
-    gallons</span> | <span id="gallonsToLitres"> ${baseValue} gallons = ${
+  } Gallons`;
+  gallonsToLitresEl.textContent = `${baseValue} Gallons = ${
     baseValue / literToGallon
-  } liters</span>`;
+  } Liters.`;
+
   // Kilo to pound and reversed.
-  massOutput.innerHTML = `<h3 id="massEl">Mass (Kilograms/Pounds)</h3>
-  <h4><span id="kiloToPound">${baseValue} kilos = ${
+  kiloToPoundEl.textContent = `${baseValue} Kilo = ${
     baseValue * kgToPound
-  } pounds</span> | <span id="poundsToKilo">${baseValue} pounds = ${
+  } Pounds.`;
+  poundToKiloEl.textContent = `${baseValue} Pounds = ${
     baseValue / kgToPound
-  } kilos</span></h4>`;
+  } Kilo.`;
 });
