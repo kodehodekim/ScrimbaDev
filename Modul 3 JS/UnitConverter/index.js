2dabcb5bb5f8502c51a9780adf43129c6127595e
kodehodekim <kim.kodehode@gmail.com>
2022-12-20 10:59:43 +0100

    Added toFixed to the converted values

--- Modul 3 JS/UnitConverter/index.js
+++ Modul 3 JS/UnitConverter/index.js
@@ -13,11 +13,6 @@ const gallonsToLitresEl = document.getElementById("gallonsToLitresElement");
 const kiloToPoundEl = document.getElementById("kiloToPoundElement");
 const poundToKiloEl = document.getElementById("poundsToKiloElement");
 
-// Calculation values
-const meterToFeet = 3.281;
-const literToGallon = 0.264;
-const kgToPound = 2.204;
-
 // Eventlistener to run function when enter is pressed.
 inputField.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
@@ -26,28 +21,31 @@ inputField.addEventListener("keypress", function (event) {
 });
 // EventListener for click on button and change the HTML.
 convertButton.addEventListener("click", function () {
+  // Calculations
+  const meterToFeet = 3.281;
+  const literToGallon = 0.264;
+  const kgToPound = 2.204;
   const baseValue = inputField.value;
+  // Meter to feet - Feet to meter
+  const meterFeet = (baseValue * meterToFeet).toFixed(2);
+  const feetMeter = (baseValue / meterToFeet).toFixed(2);
+  // Liter to gallons - Gallons to liters
+  const litersGallon = (baseValue * literToGallon).toFixed(2);
+  const gallonLiters = (baseValue / literToGallon).toFixed(2);
+  // Kilos to Pounds - Pounds to Kilos
+  const kiloPounds = (baseValue * kgToPound).toFixed(2);
+  const poundKilo = (baseValue / kgToPound).toFixed(2);
+
+  // Calculations pushed to HTML.
   // Meter to feet and reversed.
-  metertoFeetEl.textContent = `${baseValue} Meters = ${
-    baseValue * meterToFeet
-  } Feet.`;
-  feetToMeterEl.textContent = `${baseValue} Feet = ${
-    baseValue / meterToFeet
-  } Meters.`;
+  metertoFeetEl.textContent = `${baseValue} Meters = ${meterFeet} Feet.`;
+  feetToMeterEl.textContent = `${baseValue} Feet = ${feetMeter} Meters.`;
 
   // Liters to Gallons and reversed.
-  literToGallonEl.textContent = `${baseValue} Liters = ${
-    baseValue * literToGallon
-  } Gallons`;
-  gallonsToLitresEl.textContent = `${baseValue} Gallons = ${
-    baseValue / literToGallon
-  } Liters.`;
+  literToGallonEl.textContent = `${baseValue} Liters = ${litersGallon} Gallons`;
+  gallonsToLitresEl.textContent = `${baseValue} Gallons = ${gallonLiters} Liters.`;
 
   // Kilo to pound and reversed.
-  kiloToPoundEl.textContent = `${baseValue} Kilo = ${
-    baseValue * kgToPound
-  } Pounds.`;
-  poundToKiloEl.textContent = `${baseValue} Pounds = ${
-    baseValue / kgToPound
-  } Kilo.`;
+  kiloToPoundEl.textContent = `${baseValue} Kilo = ${kiloPounds} Pounds.`;
+  poundToKiloEl.textContent = `${baseValue} Pounds = ${poundKilo} Kilo.`;
 });
